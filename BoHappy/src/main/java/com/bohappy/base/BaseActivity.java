babeedfba363d2de2568b71daf99843c952d1c0c
wind <huangzefeng30@gmail.com>
2017-02-15 15:05:03 +0800

    ...

--- BoHappy/src/main/java/com/bohappy/base/BaseActivity.java
+++ BoHappy/src/main/java/com/bohappy/base/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.bohappy.base;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,8 +20,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         super.setContentView(R.layout.base_activity);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
